b7c272b7c042249a84977ed67a4410ff6e123c9b
tahaluh <thauanlucascpl@gmail.com>
2022-08-29 00:13:54 -0300

    translate update

--- src/components/quizDiv/quizDiv.js
+++ src/components/quizDiv/quizDiv.js
@@ -10,6 +10,10 @@ export default function QuizDiv(props) {
   const [answers, setAnswers] = useState([]);
   const [wasAnswered, setWasAnswered] = useState(false);
 
+  const [answersPT, setAnswersPT] = useState([]);
+  const [correctAnswerPT, setCorrectAnswerPT] = useState();
+  const [questionPT, setQuestionPT] = useState();
+
   function newQuestion() {
     setLoading(true);
     Axios.get(
@@ -22,32 +26,70 @@ export default function QuizDiv(props) {
   }
 
   useEffect(() => {
-    Axios.get(
-      "https://the-trivia-api.com/api/questions?limit=1&region=BR"
-    ).then((response) => {
-      setValues(response.data[0]);
-      setLoading(false);
-    });
+    newQuestion();
   }, []);
 
   useEffect(() => {
     if (isLoading) {
       console.log([]);
     } else {
-        setAnswers([...values.incorrectAnswers, values.correctAnswer].sort(() => .5 - Math.random()))
+      setAnswers(
+        [...values.incorrectAnswers, values.correctAnswer].sort(
+          () => 0.5 - Math.random()
+        )
+      );
+
+      // tradução pt
+
+      libreTranslate(
+        [...values.incorrectAnswers, values.correctAnswer].sort(
+          () => 0.5 - Math.random()
+        ),
+        setAnswersPT
+      );
+
+      libreTranslate(
+        values.correctAnswer,
+        setCorrectAnswerPT
+      );
+
+      libreTranslate(
+        values.question,
+        setQuestionPT
+      );
     }
   }, [values]);
 
+  async function libreTranslate(toTranslate, setToTranslate) {
+    Axios.post("https://libretranslate.de/translate", {
+      q: toTranslate,
+      source: "en",
+      target: "pt",
+      format: "text",
+      api_key: "",
+    }).then(function (response) {
+      setToTranslate(response.data.translatedText);
+    });
+  }
+
   return (
     <div className="Quiz--Div">
-      <h2 className="Quiz--Question">{values.question}</h2>
+      <h2 className="Quiz--Question">{questionPT}</h2> {/*values.question*/}
       <div className="Quiz--Options--Div">
         {true &&
-          answers.map((value, index) => {
-            return <QuizOption key={index} quizOption={value} correctAnswer={values.correctAnswer == value} wasAnswered={wasAnswered} setWasAnswered={setWasAnswered}></QuizOption>;
+          answersPT.map((value, index) => { //answers.map
+            return (
+              <QuizOption
+                key={index}
+                quizOption={value}
+                correctAnswer={correctAnswerPT == value} // correctAnswer== value
+                wasAnswered={wasAnswered}
+                setWasAnswered={setWasAnswered}
+              ></QuizOption>
+            );
           })}
       </div>
-      {wasAnswered && <NextButton newQuestion={newQuestion}/>}
+      {wasAnswered && <NextButton newQuestion={newQuestion} />}
     </div>
   );
 }
